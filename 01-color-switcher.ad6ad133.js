63b9a67eeb43368ac08eb772bfdf78a44d9f352a
Alexander-Krokhmal <103816086+Alexander-Krokhmal@users.noreply.github.com>
2022-09-24 19:03:48 +0000

    Deploying to gh-pages from @ Alexander-Krokhmal/goit-js-hw-11@4d076746e8e175474808928bf0e06c7b0fbc3973 🚀

---
+++ 01-color-switcher.ad6ad133.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]");e.addEventListener("click",(function(){const a=document.body;d=setInterval((()=>{a.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),e.disabled=!0,t.disabled=!1})),t.addEventListener("click",(function(){clearInterval(d),e.disabled=!1,t.disabled=!0}));let d=null;t.disabled=!0;
+//# sourceMappingURL=01-color-switcher.ad6ad133.js.map
